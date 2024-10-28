6ea9c121c3894569d1514c873862a7af952534c7
AhmedALHadiwi <ahmedalhadiwi04@gmail.com>
2024-10-28 17:02:52 +0300

    Add Book Store Migration

---
+++ database/migrations/2024_10_28_133604_create_authors_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->char('name',75)->nullable();
+            $table->year('yob');
+            $table->char('nationality',75);
+            $table->text('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('authors');
+    }
+};
---
+++ database/migrations/2024_10_28_133652_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Author;
+use App\Models\Language;
+use App\Models\Topic;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->char('title',150);
+            $table->foreignIdFor(Language::class)->constrained();
+            $table->foreignIdFor(Author::class)->constrained();
+            $table->foreignIdFor(Topic::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
